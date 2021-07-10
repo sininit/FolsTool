66aacc3aa5e8c7b533e34a8c8882d130189fab6f
xiaoxinwangluo <784920843@qq.com>
2021-07-10 17:20:58 +0800

    tool: fix reflects.point

--- src/top/fols/atri/reflect/Reflects.java
+++ src/top/fols/atri/reflect/Reflects.java
@@ -26,8 +26,8 @@ public class Reflects {
 
 
 
-	public ReflectPoint point(Object object) 	{ return ReflectPoint.wrapObjectOption(ReflectMatcher.DEFAULT_INSTANCE, object); }
-	public ReflectPoint point(Class<?> object) 	{ return ReflectPoint.wrapStaticOption(ReflectMatcher.DEFAULT_INSTANCE, object); }
+	public static ReflectPoint point(Object object) 	{ return ReflectPoint.wrapObjectOption(ReflectMatcher.DEFAULT_INSTANCE, object); }
+	public static ReflectPoint point(Class<?> object) 	{ return ReflectPoint.wrapStaticOption(ReflectMatcher.DEFAULT_INSTANCE, object); }
 
 
 
